e1144b7a1c83699a11a805aacfb69c74096158b7
Vincent C <vcrescente9@gmail.com>
2021-04-04 20:21:16 -0400

    preRunPY so we can run on local

--- app.py
+++ app.py
@@ -25,6 +25,12 @@ api = Api(app)
 
 jwt = JWTManager(app)  # does not create /auth, must create ourselves ex/login
 
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
 api.add_resource(UserRegister, '/register')
 # api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login', "/")
@@ -46,6 +52,5 @@ api.add_resource(Standings,  '/standings/')
 #
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
